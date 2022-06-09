6b4c4e639faac39e1b1fd43c2ff9c9f8d3d32abe
ejazahmad1995 <mhun1314@gmail.com>
2022-06-09 07:26:09 +0530

    import_food

---
+++ index.js
@@ -0,0 +1,133 @@
+
+
+
+
+let id;
+
+    async function getData(){
+
+        let query=document.getElementById("query").value
+
+        let url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+
+        let res= await fetch(url);
+
+        let data= await res.json();
+
+        console.log(data.meals);
+        
+        
+        return data.meals;
+        
+    }
+
+    async function main(){
+
+        let data = await getData();
+        console.log(data);
+
+        append(data);
+    };
+
+    function append(data){
+
+        let container=document.getElementById("results");
+
+        container.innerHTML=null;
+
+        data.forEach(function(ele,i){
+            let div = document.createElement("div");
+
+            
+            let img=document.createElement("img");
+            img.src=ele.strMealThumb
+            img.addEventListener("click",function(){
+                fooddetails(ele)
+            })
+
+            let btn = document.createElement("button");
+            btn.innerText="Delete";
+
+            btn.addEventListener("click",function(){
+                deletedetails(ele,i);
+            })
+
+            let h2 = document.createElement("h2");
+            h2.innerText=ele.strMeal;
+            console.log(h2);
+
+            div.append(img,h2,btn);
+
+            container.append(div);
+
+
+            
+
+        });
+        
+    }
+
+    let fooddetails = (ele,i)=>{
+
+        let container = document.getElementById("results");
+
+        container.innerHTML = null;
+
+
+        let img = document.createElement("img");
+        img.src=ele.strMealThumb;
+
+        let h2 = document.createElement("h2");
+            h2.innerText=ele.strMeal;
+
+
+        
+
+        let div = document.createElement("div");
+
+        div.append(img,h2);
+        container.append(div);
+
+
+
+        
+    }
+
+    let deletedetails = () =>{
+        data.splice(i,1);
+    }
+
+            
+
+                
+
+                
+
+        
+
+
+            
+
+
+            
+
+        
+            
+        
+    
+
+    
+
+    function debouncing(func,delay){
+        
+        if(id){
+            clearTimeout(id);
+        }
+        id = setTimeout(function(){
+            func();
+        },delay);
+    }
+
+    // export  { getData, append, main ,debouncing}
+
+    
---
+++ navbar.js
@@ -0,0 +1,30 @@
+function navbar(){
+    return `
+    <div id="options">
+            
+           <h3>
+              <a href="/">Home</a>
+           </h3>
+
+            <h3>
+                <a href="pizza.html">Pizza</a>
+            </h3>
+
+            <h3>
+                <a href="pasta.html">Pasta</a>
+            </h3>
+
+            <h3>
+                <a href="login.html">Login</a>
+            </h3>
+
+            <h3>
+                <a href="signup.html">Signup</a>
+            </h3>
+
+        </div>
+        `;
+
+}
+
+export default navbar;
